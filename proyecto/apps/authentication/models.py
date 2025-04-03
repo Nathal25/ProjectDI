eca4c843f7f7eeefcc23073d781ec3f62fc21900
nicolenarvaezm <nicolenar94@gmail.com>
2025-04-03 13:18:24 -0500

    "login"

--- proyecto/apps/authentication/models.py
+++ proyecto/apps/authentication/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
 class Usuario(models.Model):
     ROLES = [
@@ -10,7 +11,13 @@ class Usuario(models.Model):
     cedula = models.IntegerField(unique=True)  
     nombre = models.CharField(max_length=128)
     edad = models.IntegerField()  
-    celular = models.CharField(max_length=10) 
+    celular = models.CharField(max_length=10, unique=True,
+                               validators=[
+                                RegexValidator(
+                                regex=r'^\d{10}$',
+                                message="El número de celular debe tener exactamente 10 dígitos y no puede ser negativo."
+            )
+        ]) 
     sexo = models.CharField(max_length=10, choices=[('m', 'M'), ('f', 'F')], default='m')
     correo = models.EmailField(max_length=254, unique=True, null=True)
     rol = models.CharField(max_length=50, choices=ROLES, default='paciente')  # Cambié 'user' por 'paciente' ya que 'user' no está en choices
