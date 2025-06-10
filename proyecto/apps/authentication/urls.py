f0a7bdfa7ac64c67c880ec65e7b95684c26789a1
Juanpa0108 <jrobayomaestre@gmail.com>
2025-06-10 16:36:49 -0500

    Anuncios

--- proyecto/apps/authentication/urls.py
+++ proyecto/apps/authentication/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('registro', registrar_usuario_api, name='registro_usuario_api'),
     path('login', validar_password_usuario_api, name='validar_password_usuario_api'),
     path('validar_token', validar_token_api, name='validar_token_api'),
-    path('cambiar_punto_atencion', cambiar_punto_atencion, name='cambiar_punto_atencion_api'),
+    path('cambiar_punto_atencion', cambiar_punto_atencion, name='cambiar_punto_atencion_api')
 ]
